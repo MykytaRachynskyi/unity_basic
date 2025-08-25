814ae5b3a8b2c837fd3db9f80717bef051d0c3fc
Nikita <33069638+MykytaRachynskyi@users.noreply.github.com>
2025-08-25 19:26:14 +0200

    Object pool

---
+++ Runtime/Pooling/ObjectPool.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Basic.Pooling
+{
+    public interface IPoolElement
+    {
+        void Reset();
+    }
+
+    public class ObjectPool<T>
+        where T : IPoolElement, new()
+    {
+        public bool AssumeCapacity { get; set; }
+
+        private readonly Queue<T> _queue;
+
+        public ObjectPool(int initialCapacity = 8, bool assumeCapacity = false)
+        {
+            AssumeCapacity = assumeCapacity;
+
+            _queue = new Queue<T>(initialCapacity);
+            for (int i = 0; i < initialCapacity; ++i)
+            {
+                _queue.Enqueue(new T());
+            }
+        }
+
+        public T Get()
+        {
+            if (_queue.TryDequeue(out var element))
+            {
+                return element;
+            }
+
+            if (!AssumeCapacity)
+            {
+                return new T();
+            }
+
+            return default;
+        }
+
+        public void Return(T item)
+        {
+            item.Reset();
+            _queue.Enqueue(item);
+        }
+    }
+}
