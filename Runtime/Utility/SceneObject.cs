51fb63f0c2ec4d879a18587aa29101face6bd557
Nikita <33069638+MykytaRachynskyi@users.noreply.github.com>
2026-03-06 13:09:46 +0100

    Added api wrapper for finding objects in the scene

---
+++ Runtime/Utility/SceneObject.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace Basic
+{
+    public class SceneObject
+    {
+        public static T[] FindAll<T>()
+            where T : Object =>
+            Object.FindObjectsByType<T>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        public static T FindOne<T>(bool expectOne = true)
+            where T : Object
+        {
+            var objs = Object.FindObjectsByType<T>(
+                FindObjectsInactive.Include,
+                FindObjectsSortMode.None
+            );
+            if (objs.Length == 0)
+            {
+                return null;
+            }
+            else if (objs.Length > 1)
+            {
+                if (expectOne)
+                {
+                    Debug.LogError(
+                        $"Found {objs.Length} instances of object {typeof(T).Name}, while expected 1!"
+                    );
+                }
+                for (int i = 0; i < objs.Length; ++i)
+                {
+                    if (objs[i])
+                    {
+                        return objs[i];
+                    }
+                    else
+                    {
+                        Debug.LogError($"Found null instance of object {typeof(T).Name}");
+                    }
+                }
+                return null;
+            }
+            else
+            {
+                if (!objs[0])
+                {
+                    Debug.LogError($"Found null instance of object {typeof(T).Name}");
+                }
+                return objs[0];
+            }
+        }
+    }
+}
