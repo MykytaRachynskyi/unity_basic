27b3bc2cd4d0e55fb2dc4ca5c8a92ccdb5f30e90
Nikita <33069638+MykytaRachynskyi@users.noreply.github.com>
2026-04-12 17:37:04 +0200

    Added an overload for list shuffle that uses IRandomnessService

--- Runtime/Extensions/ExtensionMethods.cs
+++ Runtime/Extensions/ExtensionMethods.cs
@@ -1,21 +1,29 @@
 using System.Collections.Generic;
+using Basic.Randomness;
 using UnityEngine;
 
 namespace Basic
 {
-    public static class ExtensionMethods
-    {
-        public static void Shuffle<T>(this IList<T> list, System.Random random)
-        {
-            int n = list.Count;
-            for (int i = n - 1; i > 0; i--)
-            {
-                int j = random.Next(i + 1);
+	public static class ExtensionMethods
+	{
+		public static void Shuffle<T>(this IList<T> list, System.Random random)
+		{
+			var n = list.Count;
+			for (var i = n - 1; i > 0; i--)
+			{
+				var j = random.Next(i + 1);
+				(list[i], list[j]) = (list[j], list[i]);
+			}
+		}
 
-                T temp = list[i];
-                list[i] = list[j];
-                list[j] = temp;
-            }
-        }
-    }
+		public static void Shuffle<T>(this IList<T> list, IRandomnessService random)
+		{
+			var n = list.Count;
+			for (var i = n - 1; i > 0; i--)
+			{
+				var j = random.NextInt(0, i + 1);
+				(list[i], list[j]) = (list[j], list[i]);
+			}
+		}
+	}
 }
