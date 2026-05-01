a1755ed59ac2326cb2e3676030e2eac3dfe02393
Nikita <33069638+MykytaRachynskyi@users.noreply.github.com>
2026-05-01 11:21:34 +0200

    Dotween punch settings struct

---
+++ Runtime/Dotween/DotweenStructs.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Basic.DOTween
+{
+	public struct Vector3PunchSettings
+	{
+		public Vector3 Strength;
+		public float Duration;
+		public float Vibrato;
+		public float Elasticity;
+	}
+}
