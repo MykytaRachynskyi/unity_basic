9b8a4f0f0f587d5476efd8dcbaaf1b7db179a684
Nikita <33069638+MykytaRachynskyi@users.noreply.github.com>
2026-09-15 18:45:27 +0200

    Made SingleCallbackButton methods virtual

--- Runtime/UI/SingleCallbackButton.cs
+++ Runtime/UI/SingleCallbackButton.cs
@@ -9,12 +9,12 @@ namespace Basic.UI
         protected Button mainButton;
         protected System.Action _callback;
 
-        private void Awake() => mainButton.onClick.AddListener(OnClick);
+        protected virtual void Awake() => mainButton.onClick.AddListener(OnClick);
 
-        private void OnDestroy() => mainButton.onClick.RemoveListener(OnClick);
+        protected virtual void OnDestroy() => mainButton.onClick.RemoveListener(OnClick);
 
-        public void Init(System.Action callback) => _callback = callback;
+        public virtual void Init(System.Action callback) => _callback = callback;
 
-        private void OnClick() => _callback?.Invoke();
+        protected virtual void OnClick() => _callback?.Invoke();
     }
 }
